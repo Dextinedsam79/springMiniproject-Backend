b71d6bbaeab7dcc4bdffafff3b105d4c626f90ae
Oguobi Samuel Enyeribe <Dextinedsam93@gmail.com>
2025-11-12 16:58:33 +0100

    backend codebase updated

--- src/main/java/com/sol/empmgt/controller/EmployeeController.java
+++ src/main/java/com/sol/empmgt/controller/EmployeeController.java
@@ -5,6 +5,8 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 //import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -28,6 +30,11 @@ if (!list.isEmpty()) {
     System.out.println(list.get(0));
 	}
 return list;
+	}
+	@PostMapping("/employees")
+	public Employee createEmployee(@RequestBody Employee employee)
+	{
+		return repo.save(employee);
 	}
 
 }
