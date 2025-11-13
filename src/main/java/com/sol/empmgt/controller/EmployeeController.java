9ff4e4c4d544a8260fdf4a143230986ba84e35ce
Oguobi Samuel Enyeribe <Dextinedsam93@gmail.com>
2025-11-13 17:26:19 +0100

    latest backend updates committed

--- src/main/java/com/sol/empmgt/controller/EmployeeController.java
+++ src/main/java/com/sol/empmgt/controller/EmployeeController.java
@@ -3,18 +3,22 @@ package com.sol.empmgt.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 //import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.sol.empmgt.exception.EmployeeNotFoundException;
 import com.sol.empmgt.model.Employee;
 import com.sol.empmgt.repo.IEmployeeRepository;
 
 @RestController
-@CrossOrigin(origins="https://localhost:4200")
+@CrossOrigin(origins="*")
 //@RequestMapping("/employees")
 public class EmployeeController
 {
@@ -35,6 +39,24 @@ return list;
 	public Employee createEmployee(@RequestBody Employee employee)
 	{
 		return repo.save(employee);
+	}
+	
+	@GetMapping("/employees/{empId}")
+	public ResponseEntity<Employee> getEmployeeById(@PathVariable Integer empId)
+	{
+		Employee employee = repo.findById(empId).orElseThrow(()->new EmployeeNotFoundException("Employee Not Found"));
+		return ResponseEntity.ok(employee);
+	}
+	
+	@PutMapping("/employees/{empId}")
+	public ResponseEntity<Employee> updateEmployeeDetails(@PathVariable Integer empId, @RequestBody Employee employeeDetails)
+	{
+		Employee employee = repo.findById(empId).orElseThrow(()->new EmployeeNotFoundException("Employee Not Found"));
+		employee.setEmpName(employeeDetails.getEmpName());
+		employee.setEmpDesignation(employeeDetails.getEmpDesignation());
+		employee.setEmpSalary(employeeDetails.getEmpSalary());
+		repo.save(employee);
+		return ResponseEntity.ok(employee);
 	}
 
 }
